4fb6f5e378accb5e9dee334caa57ace9644cf36b
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2017-04-21 06:24:43 -0500

    Updated scripts and images

--- scripts/mandelbrot_jit.py
+++ scripts/mandelbrot_jit.py
@@ -46,14 +46,14 @@ def mandel(x, y, max_iters):
 @jit(nopython=True)
 def create_fractal(min_x, max_x, min_y, max_y, image, iters):
     height = image.shape[0]
-    width = image.shape[1]
+    width  = image.shape[1]
 
     pixel_size_x = (max_x - min_x) / width
     pixel_size_y = (max_y - min_y) / height
     for x in range(width):
         real = min_x + x * pixel_size_x
         for y in range(height):
-            imag = min_y + y * pixel_size_y
+            imag  = min_y + y * pixel_size_y
             color = mandel(real, imag, iters)
             image[y, x] = color
 
